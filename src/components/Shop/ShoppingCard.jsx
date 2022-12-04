612033f6570b03edc05850f21a723838dcaa3676
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-12-04 00:04:26 -0800

    Update ShoppingCard.jsx

--- src/components/Shop/ShoppingCard.jsx
+++ src/components/Shop/ShoppingCard.jsx
@@ -30,6 +30,7 @@ const ShoppingCard = ({ data, setShopingBasket, shopingBasket, setDetail }) => {
       },
       headers: {
         accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
         Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN,
       },
     })
